aae2500cc9bf9b91d2dec5126932600a07953b0d
PradHolla <pradholla@gmail.com>
2021-12-23 16:15:42 +0530

    First Commit

---
+++ create_table.py
@@ -0,0 +1,42 @@
+import boto3
+
+def create_devices_table(dynamodb=None):
+    dynamodb = boto3.resource(
+        'dynamodb', endpoint_url="http://localhost:8000")
+        
+    table = dynamodb.create_table(
+        TableName='Devices',
+        KeySchema=[
+            {
+                'AttributeName': 'device_id',
+                'KeyType': 'HASH'  # Partition key
+            },
+            {
+                'AttributeName': 'datacount',
+                'KeyType': 'RANGE'  # Sort key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'device_id',
+                # AttributeType defines the data type. 'S' is string type and 'N' is number type
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'datacount',
+                'AttributeType': 'N'
+            },
+        ],
+        ProvisionedThroughput={
+            # ReadCapacityUnits set to 10 strongly consistent reads per second
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10  # WriteCapacityUnits set to 10 writes per second
+        }
+    )
+    return table
+
+
+if __name__ == '__main__':
+    device_table = create_devices_table()
+    # Print tablle status
+    print("Status:", device_table.table_status)
